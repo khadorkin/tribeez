d875d047cb9d8653cd96b480126ce61b058e75e6
Antoine Rousseau <antoine@rousseau.im>
2016-02-25 07:34:28 +0100

    improved logging of express server; updated doc

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # MyTribe app
 
     npm install
-    npm run dev
-    npm run start
 
 ## API
 
-https://bitbucket.org/antoinerousseau/mytribe-api
+You need to install and start the API first: https://bitbucket.org/antoinerousseau/mytribe-api
+
+## Development
+
+    npm run dev
+    npm run start
+
+Then go to [http://localhost:3001](http://localhost:3001/)
+
+## Production
+
+    npm run deploy
+    npm install -g pm2
+    PORT=3001 pm2 start server.js --name "mytribe-app" -- --color
--- server.js
+++ server.js
@@ -2,20 +2,27 @@
 /*eslint-env node */
 'use strict'
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
 const path = require('path')
 const express = require('express')
 const compression = require('compression')
+// for logging:
+const chalk = require('chalk')
+const moment = require('moment')
 
 var app = express()
 app.use(compression())
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.join(__dirname, '/dist'), { index: false }))
 
 app.get('*', function response(req, res) {
-  console.log(req.method + ' ' + req.url)
+  const now = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
+  const ip = req.headers['cf-connecting-ip'] || req.ip
+  const country = req.headers['cf-ipcountry']
+  console.log(chalk.gray(now), chalk.green(req.method + ' ' + req.url), chalk.gray('from ' + ip + (country ? ' (' + country + ')' : '')))
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 app.listen(PORT)
-console.log('Listening on port ' + PORT)
+console.log(chalk.blue('Listening on port ' + PORT))
