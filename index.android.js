50880ba0988a1c341467176f50dc2d659ebbba50
Antoine Rousseau <antoine@rousseau.im>
2016-06-11 03:13:06 -0500

    configure Sentry for Android via Raven

--- index.android.js
+++ index.android.js
@@ -1,3 +1,12 @@
+// configure error reporting with Sentry via Raven
+import Raven from 'raven-js'
+import plugin from 'raven-js/plugins/react-native'
+plugin(Raven)
+
+import config from './src/common/config'
+import pkg from './package.json'
+Raven.config(config.sentry_dsn, {release: pkg.version}).install()
+
 import {AppRegistry} from 'react-native'
 
 import Index from './src/mobile/index'
