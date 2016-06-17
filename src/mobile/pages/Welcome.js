b1806b249f48273d59125a9239a78f668b2b820c
Antoine Rousseau <antoine@rousseau.im>
2016-06-16 21:50:27 -0500

    [mobile] show loader before register/login buttons

--- src/mobile/pages/Welcome.js
+++ src/mobile/pages/Welcome.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {StyleSheet, View, Image} from 'react-native'
 
+import {connect} from 'react-redux'
+
+import Spinner from '../components/Spinner'
 import Button from '../components/Button'
 
 import routes from '../../common/routes'
@@ -9,6 +12,10 @@ import router from '../../common/router'
 import colors from '../../common/constants/colors'
 
 class Welcome extends Component {
+  static propTypes = {
+    // redux state
+    loading: PropTypes.bool.isRequired,
+  }
 
   handleLogin() {
     router.push(routes.LOGIN)
@@ -22,8 +29,18 @@ class Welcome extends Component {
     return (
       <View style={styles.container}>
         <Image source={require('../../common/images/logo.png')} style={styles.logo} />
-        <Button id="login" onPress={this.handleLogin} />
-        <Button id="register" onPress={this.handleRegister} />
+        {
+          this.props.loading ? (
+            <View style={styles.actions}>
+              <Spinner visible={true} />
+            </View>
+          ) : (
+            <View style={styles.actions}>
+              <Button id="login" onPress={this.handleLogin} />
+              <Button id="register" onPress={this.handleRegister} />
+            </View>
+          )
+        }
       </View>
     )
   }
@@ -36,10 +53,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: colors.background,
   },
+  actions: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+  },
   logo: {
     width: 233,
     height: 205,
   },
 })
 
-export default Welcome
+const mapStateToProps = (state) => ({
+  loading: state.member.loading,
+})
+
+export default connect(mapStateToProps)(Welcome)
